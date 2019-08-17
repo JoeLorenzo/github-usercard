3eef1efe5589bc5e3d9e309f39299d32d48935d1
JoeLorenzo <Joselmedrano@gmail.com>
2019-08-17 17:50:37 -0500

    -started component

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,14 +2,6 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get('https://dog.ceo/api/breed/husky/images/random/12')
-		// Promise has resolved
-		.then((response) => {
-			console.log((response))
-})
-  .catch((error) =>{
-    console.log((error))
-  })
 /* Step 2: Inspect and study the data coming back, this is YOUR
    github info! You will need to understand the structure of this
    data in order to use it to build your component function
@@ -30,9 +22,40 @@ axios.get('https://dog.ceo/api/breed/husky/images/random/12')
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
+function userCardCreater(user){
+
+const divTag = document.createElement('div')
+const imgElement = document.createElement('img')
+const cardsClass = document.querySelector('.cards')
+imgElement.setAttribute('src', user['avatar_url'])
+divTag.classList.add('cards')
+divTag.appendChild(imgElement)
+cardsClass.appendChild(divTag)
+}
 
 const followersArray = [];
+axios.get(' https://api.github.com/users/JoeLorenzo')
+		// Promise has resolved
+		.then((response) => {
+    let user = response.data
+    console.log(user);
+    userCardCreater(user);
+})
+  .catch((error) =>{
+    console.log((error))
+  })
 
+  // function userCardCreater(user){
+  //
+  // const divTag = document.createElement('div')
+  // const imgElement = document.createElement('img')
+  // const cardsClass = document.querySelector('cards')
+  // imgElement.src = user['avatar_url']
+  // divTag.classList('card')
+  // imgElement.appendChild('divTag')
+  // divTag.appendChild('cardsClass')
+  // }
+  // userCardCreater(user)
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
